d8a45ea5e64499834948d115e721ac4576f1d8f3
Lucas Alencar <lucasbalencar05@gmail.com>
2020-05-26 11:37:56 -0300

    TODOS

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -64,7 +64,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                'carlos@especializati.com.br',
+                'admin@admin.com',
             ]);
         });
     }
